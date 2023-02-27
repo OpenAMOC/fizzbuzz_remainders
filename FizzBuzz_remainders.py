3761a8356b1d93007271bac35d93b012054059df
OpenAMOC <95543179+OpenAMOC@users.noreply.github.com>
2023-02-27 12:06:44 +0000

    Add files via upload

---
+++ FizzBuzz_remainders.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct 14 14:01:44 2022
+
+@author: wallit
+"""
+#starts loop for 1 through 100
+for n in range (1,101):
+    # % gives the remainder of a division
+    X = (n%5)
+    Y = (n%3)
+    #sets up empty output to be added to
+    output = ""
+    if Y==0:
+        output += "fizz"
+    if X==0:
+        output += "buzz"
+    if output == "":
+        #prints n if output remains empty
+        print(n)
+    else:
+        #prints the output if it has been filled by fizz, buzz or both
+        print(output)
